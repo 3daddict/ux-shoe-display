dfc02f7471748df1d37435a1aa32ed1e13e1fa3d
Michael Salvati <print@3daddict.com>
2018-11-22 12:27:55 -0800

    Schema data created

---
+++ assets/data/shoeData.js
@@ -0,0 +1,44 @@
+[{
+    shoeList: {
+        empire: {
+            id: 1,
+            model: 'empire',
+            images: {
+                black: '../images/71OKUMrnXeL._SX500._SX._UX._SY._UY_.png',
+                blue: '../images/71OKUMrnXeL._SX500._SX._UX._SY._UY_.png',
+                red: '../images/71OKUMrnXeL._SX500._SX._UX._SY._UY_.png'
+            },
+            features: 'This is where the feature details will go',
+            price: '$75.00',
+            rating: 4.5,
+            reviews: 25
+        },
+        empire: {
+            id: 1,
+            model: 'shoe2',
+            images: {
+                black: '../images/71OKUMrnXeL._SX500._SX._UX._SY._UY_.png',
+                blue: '../images/71OKUMrnXeL._SX500._SX._UX._SY._UY_.png',
+                red: '../images/71OKUMrnXeL._SX500._SX._UX._SY._UY_.png'
+            },
+            features: 'This is where the feature details will go',
+            price: '$75.00',
+            rating: 4.5,
+            reviews: 25
+        },
+        empire: {
+            id: 1,
+            model: 'shoe3',
+            images: {
+                black: '../images/71OKUMrnXeL._SX500._SX._UX._SY._UY_.png',
+                blue: '../images/71OKUMrnXeL._SX500._SX._UX._SY._UY_.png',
+                red: '../images/71OKUMrnXeL._SX500._SX._UX._SY._UY_.png'
+            },
+            features: 'This is where the feature details will go',
+            price: '$75.00',
+            rating: 4.5,
+            reviews: 25
+        }
+        
+    }
+}]
